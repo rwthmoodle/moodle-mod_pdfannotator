a1509bff84840052f2d12d5aa6cf4a74a939f5af
Tim Schroeder <t.schroeder@itc.rwth-aachen.de>
2020-08-12 20:38:09 +0200

    Release version v1.4.3

--- version.php
+++ version.php
@@ -25,6 +25,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_pdfannotator'; // Full name of the plugin (used for diagnostics).
-$plugin->version   = 2020080401; // The current module version (Date: YYYYMMDDXX).
-$plugin->release  = 'PDF Annotator v1.4 release 3';
+$plugin->version   = 2020081200; // The current module version (Date: YYYYMMDDXX).
+$plugin->release  = 'PDF Annotator v1.4 release 4';
 $plugin->requires  = 2016112900; // Requires this Moodle version.
