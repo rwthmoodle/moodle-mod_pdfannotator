b6538084ae26734789c4f18bcb788e0b0dfbf0cd
Luca Bösch <luca.boesch@bfh.ch>
2021-10-29 16:07:56 +0200

    Pdfannotator comment subscription like forum fixes #20.
    The subscription setting will distinguish between optional
    subscription, auto subscription, forced subscription and
    subscription disabled for comments, auto subscription being the
    default as it has been up to now.
    If disabled or forced, no subscribe/unsubscribe menu entry is
    shown.
    Contrary to forum if you change this in hindsight, for
    example it will not subscribe or unsubscribe all person
    who have subscribed/unsubscribed to a comment.
    Also, Behat tests are introduced hereby.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_pdfannotator';
-$plugin->version   = 2023052300;
+$plugin->version   = 2023052301;
 $plugin->release  = 'PDF Annotator v1.5 release 4';
 $plugin->requires  = 2021051700;
 $plugin->maturity  = MATURITY_STABLE;
