8ca8b0e184d295f2923d98f0594ae3863217f246
anisa kusumadewi <kusumadewi@itc.rwth-aachen.de>
2022-11-28 14:50:53 +0100

    remove string concatenation in lang-string

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_pdfannotator';
-$plugin->version   = 2022111600;
-$plugin->release  = 'PDF Annotator v1.5 release 1';
+$plugin->version   = 2022112800;
+$plugin->release  = 'PDF Annotator v1.5 release 2';
 $plugin->requires  = 2021051700;
 $plugin->maturity  = MATURITY_STABLE;
