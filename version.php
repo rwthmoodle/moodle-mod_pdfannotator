12a9e39610d753f1fe3db16ed500c924dad42fbd
Anisa <kusumadewi@itc.rwth-aachen.de>
2023-04-19 11:08:23 +0200

    make mark as don button compatible with 3.11

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_pdfannotator';
-$plugin->version   = 2023041400;
+$plugin->version   = 2023041900;
 $plugin->release  = 'PDF Annotator v1.5 release 3';
 $plugin->requires  = 2021051700;
 $plugin->maturity  = MATURITY_STABLE;
