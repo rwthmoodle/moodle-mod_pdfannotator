f464d634f5750f4cb790389463f393fa773ab427
Amrita1991 <amritad1991@gmail.com>
2025-03-17 12:42:17 +0100

    version increment and changelog updated

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_pdfannotator';
-$plugin->version   = 2025010300;
-$plugin->release  = 'PDF Annotator v1.5 release 7';
+$plugin->version   = 2025031700;
+$plugin->release  = 'PDF Annotator v1.5 release 8';
 $plugin->requires  = 2021051700;
 $plugin->maturity  = MATURITY_STABLE;
